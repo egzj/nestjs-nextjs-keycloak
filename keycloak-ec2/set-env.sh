f8bfb66d9aa13e3705825f9015d9c5ec6ad7889e
Eugene Gwee <iceified@gmail.com>
2024-12-10 11:44:55 +0800

    feat: upgrade keycloak to 26.0.5 and include providers

--- keycloak-ec2/set-env.sh
+++ keycloak-ec2/set-env.sh
@@ -2,7 +2,7 @@
 set -e
 
 export KC_HOSTNAME_URL=https://keycloak-demo01.duckdns.com
-export KEYCLOAK_FRONTEND_URL=https://keycloak-demo01.duckdns.com/auth
+# export KEYCLOAK_FRONTEND_URL=https://keycloak-demo01.duckdns.com/auth
 export KEYCLOAK_ADMIN=admin
 export KEYCLOAK_ADMIN_PASSWORD=password
 
