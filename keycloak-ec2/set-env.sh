aee89b4e46894edf4c6ef57d14d9175b00f562c2
Eugene Gwee <iceified@gmail.com>
2024-12-12 10:15:42 +0800

    config: update keycloak container env variables

--- keycloak-ec2/set-env.sh
+++ keycloak-ec2/set-env.sh
@@ -3,7 +3,7 @@ set -e
 
 export KC_HOSTNAME=https://keycloak-demo01.duckdns.com
 export KEYCLOAK_ADMIN=admin
-export KEYCLOAK_ADMIN_PASSWORD=password
+export KEYCLOAK_ADMIN_PASSWORD=admin
 
 export POSTGRES_DB=keycloak
 export POSTGRES_USER=admin
